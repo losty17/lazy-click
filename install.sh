d1b17096c311411d4af82ea76f5ed5afb78942c9
Vinícius Kappke <vinikappke@gmail.com>
2026-04-26 19:21:10 -0300

    fix: remove .go from install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 # Install script for the host machine
 # a simple go build and move to the local bin directory
-go build -o dist/ cmd/lazy-click.go
+go build -o dist/ cmd/lazy-click
 cp dist/lazy-click $HOME/.local/bin/lazy-click
